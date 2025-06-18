e15b73dfeacc8aa733ed418f61a924b9a2c4d191
maria <masha230509@gmail.com>
2025-06-18 19:42:16 +0300

    add limits

--- javascript_58-sea-fight.js
+++ javascript_58-sea-fight.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let playerBoardMatrix = [];
     let cellArray = [];
     let saveFirstCell = 0;
+    let lockColEnd = -1;
+    let lockColStart = -1;
 
     for (let i = 0; i < 10; i++) {
         let row = document.createElement("div");
@@ -14,47 +16,72 @@ document.addEventListener("DOMContentLoaded", function() {
         for (let j = 0; j < 10; j++) {
             let cell = document.createElement("div");
 
-            cell.addEventListener("click", function() {
-                let row = this.dataset.rowIndex;
-                let col = this.dataset.colIndex;
-                console.log(row, col);
-                if (saveFirstCell == 0) {
-                    saveFirstCell = this;
-                } else {
-                    let firstRow = saveFirstCell.dataset.rowIndex;
-                    let firstCol = saveFirstCell.dataset.colIndex;
-                    let secondRow = this.dataset.rowIndex;
-                    let secondCol = this.dataset.colIndex;
-                    if (firstRow == secondRow || firstCol == secondCol) {
-                        console.log("OK");
-                        if (firstRow == secondRow) {
-                            let row = firstRow;
-                            let start = Math.min(firstCol, secondCol);
-                            let end = Math.max(firstCol, secondCol);
-                            for (let col = start; col <= end; col++) {
-                                playerBoardMatrix[row][col].classList.add("asd");
-                            }
-                        }
-                        if (firstCol == secondCol) {
-                            let col = firstCol;
-                            let start = Math.min(firstRow, secondRow);
-                            let end = Math.max(firstRow, secondRow);
-                            for (let row = start; row <= end; row++) {
-                                playerBoardMatrix[row][col].classList.add("asd");
-                            }
-                        }
-                        saveFirstCell = 0;
-                    } else {
-                        alert("лінія має буть тільки вертикальна або горизонтальна");
-                        return;
-                    }
-                }
-            });
             cell.classList.add("cell");
             cell.dataset.rowIndex = i;
             cell.dataset.colIndex = j;
             row.append(cell);
             cellArray[j] = cell;
+
+            cell.addEventListener("click", function() {
+                console.log(this.dataset.rowIndex, this.dataset.colIndex);
+                if (saveFirstCell == 0) {
+                    saveFirstCell = this;
+                } else {
+                    let firstRow = Number(saveFirstCell.dataset.rowIndex);
+                    let firstCol = Number(saveFirstCell.dataset.colIndex);
+                    let secondRow = Number(this.dataset.rowIndex);
+                    let secondCol = Number(this.dataset.colIndex);
+
+                    if (firstRow == secondRow || firstCol == secondCol) {
+                        console.log("OK");
+
+                        if (firstRow == secondRow) {
+                            let start = Math.min(firstCol, secondCol); //6, 3
+                            let end = Math.max(firstCol, secondCol);//3, 6
+                            if (end - start < 4) {
+                                if (end != 9) {
+                                    lockColEnd = end + 1;
+                                } else {
+                                    lockColEnd = end;
+                                }
+
+                                if (start != 0) {
+                                    lockColStart = start - 1;
+                                } else {
+                                    lockColStart = start;
+                                }
+
+                                for (let col = start; col <= end; col++) {
+                                    playerBoardMatrix[firstRow][col].classList.add("asd");
+                                }
+                                for (let col = lockColStart; col <= lockColEnd; col++) {
+                                    playerBoardMatrix[firstRow][col].dataset.lock = true;
+                                    if (firstRow != 0) {
+                                        playerBoardMatrix[firstRow - 1][col].dataset.lock = true;
+                                    }
+                                    if (firstRow != 9) {
+                                        playerBoardMatrix[firstRow + 1][col].dataset.lock = true;
+                                    }
+                                }
+                            }
+                        }
+                        else if (firstCol == secondCol) {
+                            let start = Math.min(firstRow, secondRow);
+                            let end = Math.max(firstRow, secondRow);
+                            if (end - start < 4) {
+                                for (let row = start; row <= end; row++) {
+                                    playerBoardMatrix[row][firstCol].classList.add("asd");
+                                }
+                            }
+                        }
+                        saveFirstCell = 0;
+                    } else {
+                        alert("лінія має буть тільки вертикальна або горизонтальна");
+                        saveFirstCell = 0;
+                        // return;
+                    }
+                }
+            });
         }
         playerBoardMatrix[i] = cellArray
     }
@@ -77,5 +104,5 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         // playerBoardMatrix[i] = cellArray
     }
-    console.log(playerBoardMatrix);
+    // console.log(enemyBoardMatrix);
 });
